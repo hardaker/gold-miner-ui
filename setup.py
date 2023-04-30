c6fcad31c9f01506db166510f2939c2c2fdc0365
Wes Hardaker <opensource@hardakers.net>
2023-04-30 06:31:53 -0700

    version bump again

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gold-miner-ui",
-    version="1.4",
+    version="1.5",
     author="Wes Hardaker",
     author_email="opensource@hardakers.net",
     description="A UI plugin that adds graphical extensions to gold-miner",
